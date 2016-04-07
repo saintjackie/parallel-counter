e8014db992d5d9695b7886b99e84eb510d081f12
Jakub Holy <saintjackie@gmail.com>
2016-04-07 11:34:44 +0200

    counter changed

--- src/parallel/counter/Counter.java
+++ src/parallel/counter/Counter.java
@@ -5,27 +5,28 @@ package parallel.counter;
  * @author Jakub Holy
  */
 public class Counter implements Runnable{
+    
     private static final Object LOCK = new Object();
     private static int value = -1;
 
     @Override
     public void run() {
-        for(long i = 0;i<100;i++){
-            increaseValue();
-            try {
-                Thread.sleep(120);
-            } catch (InterruptedException ex) {
-            }
+        
+        while(true){
+            for(long l=0;l<100000000l;l++);
+            synchronized (LOCK) {
+            if (value >= 50)
+                break;
+                value++;
+                System.out.println(Thread.currentThread().getName() + " : " +  value);
+            
+        }
+            for(long l=0;l<10000000l;l++);
         }
     }
     
     private void increaseValue(){
-        synchronized (LOCK) {
-            if (value < 50) {
-                value++;
-                System.out.println(Thread.currentThread().getName() + " : " +  value);
-            }
-        }
+        
     }
     
     
